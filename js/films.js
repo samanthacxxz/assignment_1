9c2be66324673fd946da66bc339222e3c641fc18
samanthacxxz <144262012+samanthacxxz@users.noreply.github.com>
2024-01-26 21:58:44 +0100

    retrieving data, storing in local storage, rendering

---
+++ js/films.js
@@ -0,0 +1,22 @@
+// retrieving data from the Star Wars film resources
+
+const filmData = () => {
+    fetch("http://swapi.dev/api/films")
+    .then(response => response.json())
+    .then (data => console.log(data.results))
+}
+
+filmData();
+
+// saving the film data to the local storage
+
+const saveFilmData = (data) => {
+    localStorage.setItem("filmData", JSON.stringify(data));
+};
+
+//getting the data from the local storage when the page is loaded
+
+const starwarsFilms = JSON.parse(localStorage.getItem("filmData"));
+document.addEventListener("DOMContentLoaded", () => {
+    renderData(starwarsFilms);
+});
