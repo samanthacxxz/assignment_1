2a6234928a0493a085d63b23e5f19e8cc9d8ff23
Samantha Cruz <144262012+samanthacxxz@users.noreply.github.com>
2024-01-31 13:03:52 +0100

    displayed planets data to page

--- js/planets.js
+++ js/planets.js
@@ -9,6 +9,43 @@ fetch('http://swapi.dev/api/planets')
 .then(response => response.json())
 .then(planets => {
     console.log(planets.results)
-    renderPeopleData(planets.results)
+    renderPlanetsData(planets.results)
    
 });
+
+function renderPlanetsData(planets) {
+    planets.forEach(planet => {
+        // creating a list for each film
+        const li = document.createElement('li');
+
+        const planetName = document.createElement('div');
+        const planetClimate = document.createElement('div');
+        const planetGravity = document.createElement('div');
+        const planetSurfaceWater = document.createElement('div');
+        const planetTerrain = document.createElement('div');
+
+        // adding classes
+        
+        li.classList.add('planetItem')
+
+        planetName.classList.add('planet-name');
+        planetClimate.classList.add('planet-climate');
+        planetGravity.classList.add('planet-gravity');
+        planetSurfaceWater.classList.add('planet-surface-water');
+        planetTerrain.classList.add('planet-terrain');
+        
+        // appending the elements to the page 
+
+        ul.append(li)
+        li.append(planetName, planetClimate, planetGravity, planetSurfaceWater, planetTerrain);
+
+        //adding content 
+
+        planetName.textContent = `Name: ${planet.name}`;
+        planetClimate.textContent = `Climate: ${planet.climate}`;
+        planetGravity.textContent = `Gravity: ${planet.gravity}`;
+        planetSurfaceWater.textContent = `Surface Water ${planet.surface_water}`;
+        planetTerrain.textContent = `Terrain: ${planet.terrain}`;
+
+    });
+}
