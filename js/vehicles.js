275bcf1dcb2b578c4c959524f8207897c60291fb
Samantha Cruz <144262012+samanthacxxz@users.noreply.github.com>
2024-01-31 13:28:10 +0100

    vehicles data displayed to html

--- js/vehicles.js
+++ js/vehicles.js
@@ -12,3 +12,40 @@ fetch('http://swapi.dev/api/vehicles')
     renderVehiclesData(vehicles.results)
    
 });
+
+function renderVehiclesData(vehicles) {
+    vehicles.forEach(vehicle => {
+        // creating a list for each film
+        const li = document.createElement('li');
+
+        const vehicleName = document.createElement('div');
+        const vehicleModel = document.createElement('div');
+        const vehicleManufacturer = document.createElement('div');
+        const vehiclePassengers = document.createElement('div');
+        const vehicleVehicleClass = document.createElement('div');
+
+        // adding classes
+        
+        li.classList.add('planetItem')
+
+        vehicleName.classList.add('vehicle-name');
+        vehicleModel.classList.add('vehicle-model');
+        vehicleManufacturer.classList.add('vehicle-manufacturer');
+        vehiclePassengers.classList.add('vehicle-passengers');
+        vehicleVehicleClass.classList.add('vehicle-vehicle-class');
+        
+        // appending the elements to the page 
+
+        ul.append(li)
+        li.append(vehicleName, vehicleModel, vehicleManufacturer, vehiclePassengers, vehicleVehicleClass);
+
+        //adding content 
+
+        vehicleName.textContent = `Name: ${vehicle.name}`;
+        vehicleModel.textContent = `Model: ${vehicle.model}`;
+        vehicleManufacturer.textContent = `Manufacturer: ${vehicle.manufacturer}`;
+        vehiclePassengers.textContent = `Passengers ${vehicle.passengers}`;
+        vehicleVehicleClass.textContent = `Vehicle Class: ${vehicle.vehicle_class}`;
+
+    });
+}
