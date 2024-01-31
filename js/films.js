75f3e3206feb7181919663ca6b724887d75f168f
Samantha Cruz <144262012+samanthacxxz@users.noreply.github.com>
2024-01-31 23:24:59 +0100

    saved file

--- js/films.js
+++ js/films.js
@@ -10,15 +10,16 @@ fetch('http://swapi.dev/api/films')
 .then(films => {
     console.log(films.results)
     renderFilmData(films.results)
-
 });
+
+// rendering the data retrieved from API, appending to the page
 
 function renderFilmData(films) {
     films.forEach(film => {
         // creating a list for each film
         const li = document.createElement('li');
 
-        const filmTitle = document.createElement('div');
+        const filmTitle = document.createElement('button');
         const filmProducer = document.createElement('div');
         const filmReleaseDate = document.createElement('div');
         const filmDirector = document.createElement('div');
@@ -33,7 +34,14 @@ function renderFilmData(films) {
         filmReleaseDate.classList.add('film-releasedate');
         filmDirector.classList.add('film-director');
         filmEpisodeID.classList.add('film-episodeID');
-        
+
+        // not displaying the property to page yet except for the title
+
+        filmProducer.classList.add('film-property');
+        filmReleaseDate.classList.add('film-property');
+        filmDirector.classList.add('film-property');
+        filmEpisodeID.classList.add('film-property');
+
         // appending the elements to the page 
 
         ul.append(li)
@@ -41,11 +49,25 @@ function renderFilmData(films) {
 
         //adding content 
 
-        filmTitle.textContent = `Title: ${film.title}`;
+        filmTitle.textContent = film.title;
         filmProducer.textContent = `Producer: ${film.producer}`;
         filmReleaseDate.textContent = `Release Date: ${film.release_date}`;
         filmDirector.textContent = `Director ${film.director}`;
         filmEpisodeID.textContent = `Èpisode ID: ${film.episode_id}`;
 
+        // display properties when clicking the title button
+
+
+        filmTitle.addEventListener('click', () => {
+
+            filmTitle.classList.toggle('film-property--visible');
+            filmProducer.classList.toggle('film-property--visible');
+            filmReleaseDate.classList.toggle('film-property--visible');
+            filmDirector.classList.toggle('film-property--visible');
+            filmEpisodeID.classList.toggle('film-property--visible');
+
+        });
     });
+    ;
 } 
+
