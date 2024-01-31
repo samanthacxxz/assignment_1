eeab9e67f0143357c3a5cd30823243d55f2ef994
Samantha Cruz <144262012+samanthacxxz@users.noreply.github.com>
2024-01-31 22:05:02 +0100

    code for displaying properties after clicking on title

--- js/films.js
+++ js/films.js
@@ -10,7 +10,6 @@ fetch('http://swapi.dev/api/films')
 .then(films => {
     console.log(films.results)
     renderFilmData(films.results)
-    showDetails(films.results)
 });
 
 // rendering the data retrieved from API, appending to the page
@@ -30,12 +29,19 @@ function renderFilmData(films) {
         
         li.classList.add('filmItem')
 
-        filmTitle.classList.add('film-title', 'title-button');
+        filmTitle.classList.add('film-title');
         filmProducer.classList.add('film-producer');
         filmReleaseDate.classList.add('film-releasedate');
         filmDirector.classList.add('film-director');
         filmEpisodeID.classList.add('film-episodeID');
-        
+
+        // not displaying the property to page yet except for the title
+
+        filmProducer.classList.add('film-property');
+        filmReleaseDate.classList.add('film-property');
+        filmDirector.classList.add('film-property');
+        filmEpisodeID.classList.add('film-property');
+
         // appending the elements to the page 
 
         ul.append(li)
@@ -43,33 +49,25 @@ function renderFilmData(films) {
 
         //adding content 
 
-        filmTitle.textContent = `Title: ${film.title}`;
+        filmTitle.textContent = film.title;
         filmProducer.textContent = `Producer: ${film.producer}`;
         filmReleaseDate.textContent = `Release Date: ${film.release_date}`;
         filmDirector.textContent = `Director ${film.director}`;
         filmEpisodeID.textContent = `Èpisode ID: ${film.episode_id}`;
 
-        // not displaying the property except for the title
+        // display properties when clicking the title button
 
-        filmProducer.style.display = 'none';
-        filmReleaseDate.style.display = 'none';
-        filmDirector.style.display = 'none';
-        filmEpisodeID.style.display = 'none';
 
+        filmTitle.addEventListener('click', () => {
+
+            filmTitle.classList.toggle('film-property--visible');
+            filmProducer.classList.toggle('film-property--visible');
+            filmReleaseDate.classList.toggle('film-property--visible');
+            filmDirector.classList.toggle('film-property--visible');
+            filmEpisodeID.classList.toggle('film-property--visible');
+
+        });
     });
-
-
-    
+    ;
 } 
 
-// display properties when clicking the title button
-
-filmTitleButton = showDetails(film.title)
-    
-filmTitleButton.addEventlistener('click', showDetails)
-
-function showDetails(film) {
-    filmsArray = film.entries(film);
-    console.log(filmsArray)
-}
-showDetails(film);
