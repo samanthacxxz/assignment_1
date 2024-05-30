5a3764e0576e9e74e52181d05e2c303654428ffd
Samantha <144262012+samanthacxxz@users.noreply.github.com>
2024-05-31 00:30:53 +0200

    function display vehicles

--- js/app.js
+++ js/app.js
@@ -40,6 +40,14 @@ planetsCategoryButton.addEventListener('click', ()=> {
     displayPlanetsData();
 })
 
+vehiclesCategoryButton.addEventListener('click', ()=> {
+    filmDisplay.style.display = 'none';
+    peopleDisplay.style.display = 'none';
+    planetsDisplay.style.display = 'none';
+    vehiclesDisplay.style.display = 'flex';
+    displayVehiclesData();
+})
+
 // DISPLAY FILM SECTION
 function displayFilmData() {
 
@@ -193,4 +201,54 @@ function displayPlanetsData() {
             planetTerrain.textContent = `Terrain: ${planet.terrain}`;
         });
     };
+}
+
+// DISPLAY VEHICLES SECTION
+
+function displayVehiclesData() {
+    // retrieve data from HTTP - vehicles 
+    fetch('http://swapi.dev/api/vehicles')
+    .then(response => response.json())
+    .then(vehicles => {
+        console.log(vehicles.results)
+        renderVehiclesData(vehicles.results)
+    });
+
+    const ul = document.querySelector('.vehicles-ul');
+
+    function renderVehiclesData(vehicles) {
+        vehicles.forEach(vehicle => {
+            // creating a list for each vehicle
+            const li = document.createElement('li');
+    
+            const vehicleName = document.createElement('div');
+            const vehicleModel = document.createElement('div');
+            const vehicleManufacturer = document.createElement('div');
+            const vehiclePassengers = document.createElement('div');
+            const vehicleVehicleClass = document.createElement('div');
+    
+            // adding classes
+            
+            li.classList.add('item')
+    
+            vehicleName.classList.add('vehicle-name');
+            vehicleModel.classList.add('vehicle-model');
+            vehicleManufacturer.classList.add('vehicle-manufacturer');
+            vehiclePassengers.classList.add('vehicle-passengers');
+            vehicleVehicleClass.classList.add('vehicle-vehicle-class');
+    
+            // appending the elements to the page 
+    
+            ul.append(li)
+            li.append(vehicleName, vehicleModel, vehicleManufacturer, vehiclePassengers, vehicleVehicleClass);
+    
+            //adding content 
+    
+            vehicleName.textContent = vehicle.name;
+            vehicleModel.textContent = `Model: ${vehicle.model}`;
+            vehicleManufacturer.textContent = `Manufacturer: ${vehicle.manufacturer}`;
+            vehiclePassengers.textContent = `Passengers ${vehicle.passengers}`;
+            vehicleVehicleClass.textContent = `Vehicle Class: ${vehicle.vehicle_class}`;
+        });
+    }
 }
