0d79cd17f26259e6567e4832017cc6f936d9263a
Samantha <144262012+samanthacxxz@users.noreply.github.com>
2024-05-30 23:33:31 +0200

    function to display film

--- js/app.js
+++ js/app.js
@@ -3,10 +3,22 @@
 const selectCategoriesContainer = document.querySelector('.categories-section')
 const categoriesUl = document.querySelector('.starwars_categories')
 
-const filmCategoryButton = document.querySelector('.')
-const peopleCategoryButton = document.querySelector('.')
-const planetsCategoryButton = document.querySelector('.')
-const vehiclesCategoryButton = document.querySelector('.')
+const filmCategoryButton = document.querySelector('.films-button')
+const peopleCategoryButton = document.querySelector('.people-button')
+const planetsCategoryButton = document.querySelector('.planets-button')
+const vehiclesCategoryButton = document.querySelector('.vehicles-button')
 
 const filmDisplay = document.querySelector('.film-display');
+const peopleDisplay = document.querySelector('.people-display');
+const planetsDisplay = document.querySelector('.planets-display');
+const vehiclesDisplay = document.querySelector('.vehicles-display');
 
+
+// EVENT LISTENERS TO BUTTONS 
+
+filmCategoryButton.addEventListener('click', ()=> {
+    DisplayFilmData();
+})
+
+
+//
